623a9959dbacbbc4d84cef4fa7000198d067eca7
Jitesh Gadage <gadagejitesh16@gmail.com>
2022-10-01 01:11:38 +0530

    added calculaor function and previous calcultion pending

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,13 +1,25 @@
 
+
+
 function insert(num){
+  document.querySelector('.textview').value += num
+console.log(num)
+    return num
   }
   
   function equals(){
     
-      
+    let x = document.querySelector('.textview').value 
+
+    let y = eval(x);
+
+    document.querySelector('.textview').value = y
+
+    return y
   }
   
   function clean(){
+    document.querySelector('.textview').value = ''
   }
   
   function back(){
