1d15ef6a7a41824123763f7ce6d0f0a297b0d3d1
Jitesh Gadage <gadagejitesh16@gmail.com>
2023-01-24 20:01:49 +0530

    js code fixed

--- Age-Calculator/app.js
+++ Age-Calculator/app.js
@@ -1,84 +1,41 @@
-const error =  document.querySelector(".error");
-const age =  document.querySelector(".age");
-
-
-
-function getAge(){
-    
-    let yourAge = new Date(document.getElementById("dob").value);  //gets input value in  indian standard time
-// console.log(yourAge)
-    let  presetDate = new Date();    //gets preset value in  indian standard time
-    // console.log(presetDate  )   
-
-// get your date month and  year
-
-    let myDate = yourAge.getDate()
-    let myMonth = yourAge.getMonth()
-    let myYear = yourAge.getFullYear()
-        // console.log(myDate)
-        // console.log(myMonth)
-        // console.log(myYear)
-        calcAge(presetDate,myDate,myMonth,myYear)
-}
-
-function calcAge(presetDate,myDate,myMonth,myYear){ 
-    
-    let todaysDate = presetDate.getDate();
-    let todaysMonth = presetDate.getMonth();
-    let todaysYear = presetDate.getFullYear()
-    // console.log(todaysDate)
-    // console.log(todaysMonth)
-    // console.log(todaysYear)
-
-// check if selected age is eqaul to curret age 
-
-    if(myDate == todaysDate && myMonth == todaysMonth && myYear == todaysYear){
-        age.style.display = "none";
-        error.style.display = "block";
-        error.textContent = "You are not yet born..!!! Time Traveller";
+// get age input value convert in standerd date.
+// comparing the standerd date with current date,
+// if input date is greated then current date
+// show alert , reset input date and date of birth
+// else substract current date  with  input standerd date
+// and pass to calcAge()
+function getAge() {
+    const dobInput = document.getElementById("dob");
+    dobInput.value = new Date();
+    dobInput.addEventListener("change", (e) => {
+      dob = new Date(e.target.value);
+      if (dob > new Date()) {
+        alert("enter valid date of birth");
+        dobInput.value = "";
+        document.getElementById("years").innerHTML = "";
+        document.getElementById("months").textContent = "";
+        document.getElementById("days").textContent = "";
         return;
-    }
-
-// check if selected age is greater than curret age 
-
-
-    if(myDate > todaysDate || myMonth > todaysMonth || myYear > todaysYear ){
-        age.style.display = "none";
-        error.style.display = "block";
-        error.textContent = "Invalid age enterd...!!! Time Traveller";
-        return;
-    }
-
-    let mytotalYear = todaysYear - myYear
-    let mytotalMonth = todaysMonth - myMonth
-    let mytotalDays = todaysDate - myDate
-    let mayage = `${todaysDate - myDate} / ${todaysMonth - myMonth} / ${todaysYear - myYear} ` 
-    console.log(mayage)
-
-    error.style.display = "block";
-    // console.log(mytotalMonth)
-    // console.log(mytotalDays)
-
-    displayAge(mytotalYear,mytotalMonth,mytotalDays)
-
-}
-
-function displayAge(mytotalYear,mytotalMonth,mytotalDays){
-
-    let year = document.getElementById("years");
-    let months = document.getElementById("months");
-    let days = document.getElementById("days");
-    let disc = document.querySelector(".desc")
-
-
-    year.innerText = mytotalYear
-    months.innerText = mytotalMonth
-    days.innerText = mytotalDays
-
-
- 
-    disc.innerText = `Your Age is : ${mytotalYear}`
-    
-    
-}
-
+      }
+      const stdDate = new Date(new Date() - dob);
+      calcAge(stdDate);
+    });
+  }
+  getAge();
+  
+  //  convert Standerd date to day, month,year
+  // and pass to displayAge()
+  
+  function calcAge(stdDate) {
+    const month = stdDate.getMonth();
+    const day = stdDate.getDate() - 1;
+    const year = stdDate.getFullYear() - 1970;
+    displayAge(year, month, day);
+  }
+  
+  //  finaly display the age
+  function displayAge(year, month, day) {
+    document.getElementById("years").innerHTML = year;
+    document.getElementById("months").textContent = month;
+    document.getElementById("days").textContent = day;
+  }
