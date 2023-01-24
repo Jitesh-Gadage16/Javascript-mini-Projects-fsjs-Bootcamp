e7500aa53598b9c7eff9323dacb2e5a348127ba5
Jitesh Gadage <gadagejitesh16@gmail.com>
2023-01-24 20:00:38 +0530

    qr code css and js changes

--- 07QRCodeGenerator - API/App.js
+++ 07QRCodeGenerator - API/App.js
@@ -1,58 +1,30 @@
-let btn = document.querySelector(".submit");
-let qr_code_element = document.querySelector("#img");
+const input = document.querySelector("#input");
+const submit = document.querySelector("#submit");
+const displayQR = document.querySelector(".qr-code");
 
-btn.addEventListener("click", () => {
-    let user_input = document.querySelector(".data");
-    if (user_input.value != "") {
-        if (qr_code_element.childElementCount == 0) {
-            generate(user_input);
-        } else {
-            qr_code_element.innerHTML = "";
-            generate(user_input);
-        }
-    } else {
-       console.log("fail")
-    }
-});
-
-function generate(user_input) {
-    document.querySelector(".error").style = "display: none;";
-
-    var qrcode = new QRCode(qr_code_element, {
-        text: `${user_input.value}`,
-        width: 180, //128
-        height: 180,
-        colorDark: "#000000",
-        colorLight: "#ffffff",
-        correctLevel: QRCode.CorrectLevel.H,
-    });
-
-    let download = document.createElement("button");
-    qr_code_element.appendChild(download);
-
-    let download_link = document.createElement("a");
-    download_link.setAttribute("download", "qr_code.png");
-    download_link.innerHTML = `Download <i class="fa-solid fa-download"></i>`;
-
-    download.appendChild(download_link);
-
-    let qr_code_img = document.querySelector(".qr-code img");
-    let qr_code_canvas = document.querySelector("canvas");
-
-    if (qr_code_img.getAttribute("src") == null) {
-        setTimeout(() => {
-            download_link.setAttribute("href", `${qr_code_canvas.toDataURL()}`);
-        }, 300);
-    } else {
-        setTimeout(() => {
-            download_link.setAttribute(
-                "href",
-                `${qr_code_img.getAttribute("src")}`
-            );
-        }, 300);
-    }
+function generateQR(text) {
+  displayQR.innerHTML = "";
+  new QRCode(displayQR, {
+    text: text,
+    width: 128,
+    height: 128,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H,
+  });
 }
 
-generate({
-    value: "https://qr-codes.vercel.app",
+submit.addEventListener("click", () => {
+  const text = input.value;
+  if (text == "") {
+    return alert("enter text or url");
+  }
+  const qrCode = generateQR(text);
+  displayQR.setAttribute("src", qrCode);
 });
+
+function demoQr() {
+  input.value = "demo";
+  generateQR("demo");
+}
+demoQr();
